3653252059b9df8c46c07e06cc42c8beadccd731
jaekim3220 <jehyunkim9@gmail.com>
2023-07-15 09:54:10 +0900

    05-워드클라우드(한글).ipynb 복습, helper.py 모듈 갱신

--- helper.py
+++ helper.py
@@ -33,3 +33,19 @@ def replaceOutlier(df, fieldName):
         cdf.loc[cdf[f] > 결측치경계[1], f] = np.nan
 
     return cdf
+
+
+def clearStopwords(nouns, stopwords_file_path="wordcloud/stopwords-ko.txt"):
+    with open(stopwords_file_path, 'r', encoding='utf-8') as f:
+        stopwords = f.readlines()
+        
+        for i, v in enumerate(stopwords):
+            stopwords[i] = v.strip()
+
+    data_set = []
+
+    for v in nouns:
+        if v not in stopwords:
+            data_set.append(v)
+
+    return data_set
